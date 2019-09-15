f988dd6de3c9e0738979645ac4ff6d3eefc99a6e
Guillaume Vincent <g_vincen@etud.insa-toulouse.fr>
2019-09-15 22:04:16 +0200

    Added getTimeOfAscendingNode function

--- toolkit/calculations.py
+++ toolkit/calculations.py
@@ -1,5 +1,6 @@
 import krpc
 import math
+from time import sleep
 
 def getOrbitalPeriod(sma,mu):
     """Return the orbital period of a vessel, using the semi major axis.
@@ -14,3 +15,18 @@ def getSemiMajorAxis(period,mu):
     """
     sma = ((mu * period**2) / (4 * math.pi**2)) ** (1 / 3)
     return sma
+
+def getTimeOfAscendingNode(conn):
+    """Return the ut of the next ascending node.
+
+    """
+    mj = conn.mech_jeb
+    planner = mj.maneuver_planner
+    mjInc = planner.operation_inclination
+
+    mjInc.time_selector.time_reference = mj.TimeReference.eq_ascending
+    anNode = mj_inc.make_node()
+    anUT = anNode.ut
+    sleep(0.1)
+    anNode.remove()
+    return anUT
