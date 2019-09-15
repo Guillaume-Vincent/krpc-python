4c1433d093948977f9952fb0e2acf5e30eebc5ef
Guillaume Vincent <g_vincen@etud.insa-toulouse.fr>
2019-09-15 18:37:03 +0200

    Added maneuvers.py

---
+++ toolkit/maneuvers.py
@@ -0,0 +1,81 @@
+import krpc
+from time import sleep
+
+def executeNextNode(conn,tolerance,leadTime):
+    """Execute the next node.
+
+    """
+    executor = conn.mech_jeb.node_executor
+    vsl = conn.space_center.active_vessel
+    executorStatus = conn.add_stream(getattr,executor,'enabled')
+    nodeList = conn.add_stream(getattr,vsl.control,'nodes')
+
+    executor.tolerance = tolerance
+    executor.lead_time = lead_time
+    executor.execute_one_node()
+    sleep(1)
+    while (executorStatus() is True) or (nodeList() != []):
+        pass
+
+def manPeriapsis(conn,newPeriapsis,atApsis,tolerance=0.01,leadTime=5):
+    """Change the periapsis of the orbit.
+
+    Parameters:
+    -newPeriapsis : the new desired periapsis of the orbit
+    -atApsis : location of the burn ('apoapsis','periapsis')
+    -tolerance : tolerance of the mechjeb node executor
+    -leadTime : lead time of warp before burn time
+    """
+    mj = conn.mech_jeb
+    planner = mj.maneuver_planner
+    mjPeri = planner.operation_periapsis
+
+    if atApsis = 'apoapsis':
+        mjPeri.time_selector.time_reference = mj.TimeReference.apoapsis
+    elif atApsis = 'periapsis':
+        mjPeri.time_selector.time_reference = mj.TimeReference.periapsis
+    mjPeri.new_periapsis = newPeriapsis
+    mjPeri.make_node()
+    executeNextNode(conn,tolerance,leadTime)
+
+def manApoapsis(conn,newPeriapsis,atApsis,tolerance=0.01,leadTime=5):
+    """Change the apoapsis of the orbit.
+
+    Parameters:
+    -newApoapsis : the new desired apoapsis of the orbit
+    -atApsis : location of the burn ('apoapsis','periapsis')
+    -tolerance : tolerance of the mechjeb node executor (default : 0.01)
+    -leadTime : lead time of warp before burn time (default : 5)
+    """
+    mj = conn.mech_jeb
+    planner = mj.maneuver_planner
+    mjApo = planner.operation_apoapsis
+
+    if atApsis = 'apoapsis':
+        mjApo.time_selector.time_reference = mj.TimeReference.apoapsis
+    elif atApsis = 'periapsis':
+        mjApo.time_selector.time_reference = mj.TimeReference.periapsis
+    mjApo.new_apoapsis = newApoapsis
+    mjApo.make_node()
+    executeNextNode(conn,tolerance,leadTime)
+
+def opSemiMajorAxis(conn,newSMA,atApsis,tolerance=0.01,leadTime=5):
+    """Changes the semi major axis of the orbit.
+
+    Parameters:
+    -newSMA : the new desired semi major axis of the orbit
+    -atApsis : location of the burn ('apoapsis','periapsis')
+    -tolerance : tolerance of the mechjeb node executor (default : 0.01)
+    -leadTime : lead time of warp before burn time (default : 5)
+    """
+    mj = conn.mech_jeb
+    planner = mj.maneuver_planner
+    mjSMA = planner.operation_semi_major
+
+    if atApsis == 'apoapsis':
+        mjSMA.time_selector.time_reference = mj.TimeReference.apoapsis
+    elif atApsis == 'periapsis':
+        mjSMA.time_selector.time_reference = mj.TimeReference.periapsis
+    mj_sma.new_semi_major_axis = newSMA
+    mj_sma.make_node()
+    execute_next_node(conn,tolerance,leadTime)
