bb25aa9cd69557acc204f60973c605cd5b2964c3
Guillaume Vincent <g_vincen@etud.insa-toulouse.fr>
2019-09-16 12:06:18 +0200

    Added convertTime, targetBody, findVessel

--- toolkit/misc.py
+++ toolkit/misc.py
@@ -1,6 +1,6 @@
 import krpc
 
-def countdown(conn,t):
+def countdown(conn, t):
     """Create a t-seconds countdown.
 
     """
@@ -13,3 +13,44 @@ def countdown(conn,t):
             pass
         T += 1
     ut.remove()
+
+def convertTime(ut):
+    """Convert a time in seconds to a Y D HH:MM:SS format (KSP game time).
+
+    """
+    ut_s = int(ut)
+    ut_m = int(ut_s / 60)
+    ut_h = int(ut_m / 60)
+    ut_d = 1 + int(ut_h / 6)
+    ut_y = 1 + int(ut_d / 427)
+
+    ut_s = ut_s % 60
+    ut_m = ut_m % 60
+    ut_h = ut_h % 6
+    ut_d = ut_d % 427
+
+    Y = "Y" + str(ut_y)
+    D = "D" + str(ut_d)
+    HH = '{:02}'.format(ut_h)
+    MM = '{:02}'.format(ut_m)
+    SS = '{:02}'.format(ut_s)
+
+    return (Y + " " + D + " " + HH + ":" + MM + ":" + SS)
+
+def targetBody(conn, body):
+    """Target the desired body.
+
+    """
+    for n in conn.space_center.bodies:
+        if n == body:
+            conn.space_center.target_body = conn.space_center.bodies.get(n)
+    return conn.space_center.target_body
+
+def findVessel(conn, vesselName):
+    """Return the first vessel that has the given name.
+
+    """
+    for V in conn.space_center.vessels:
+        if V.name == vesselName:
+            return V
+    return None
