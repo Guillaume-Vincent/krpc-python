e91354dfdd90d60162663e11112e8d1472562109
Guillaume Vincent <g_vincen@etud.insa-toulouse.fr>
2019-09-17 00:19:32 +0200

    Created better panel and text handling

--- toolkit/ui.py
+++ toolkit/ui.py
@@ -1,34 +1,44 @@
 import krpc
 
+class Panel:
+    """Define a panel and its components.
+
+    """
+    def __init__(self, conn, panelSize, panelPosition, panelName):
+        self.panelSize = panelSize
+        self.panelPosition = panelPosition
+        self.panelName = panelName
+
+        self.canvas = conn.ui.add_canvas()
+        self.panel = self.canvas.add_panel()
+        self.elements = []
+
+        self.panel.rect_transform.size = self.panelSize
+        self.panel.rect_transform.position = self.panelPosition
+
+    def addLine(self, line, color=(0,0,0), size=20, font='Corbel Bold',
+                position=(0,0),anchor='middle_center',style='normal'):
+        text = self.panel.add_text(line)
+        self.elements.append(text)
+
+        text.rect_transform.position = position
+        text.rect_transform.size = size
+        text.font = font
+        text.size = size
+        text.style = style
+        text.color = tuple([c/256 for c in color])
+
+    def remove(self):
+        self.panel.remove()
+        self.canvas.remove()
+        for elem in self.elements:
+            elem.remove()
+
 def mainMenuUI(conn):
     """Create the UI of the main menu.
 
-    Return all the elements of the UI for future deletion.
     """
-    #Create canvas and panel
-    canvas = conn.ui.add_canvas()
-    panel = canvas.add_panel()
-
-    #Adjust the size of the panel
-    rect = panel.rect_transform
-    rect.size = (400, 750)
-
-    #All the different lines that will be writen in the panel
-    # TODO: Static & dynamic ?
-    # TODO: Funcion to better handle text creation
-    linesList = ["Main Menu"]
-
-    textList = []
-    for line in linesList:
-        textList.append(panel.add_text(line))
-
-    textList[0].font = 'Corbel Bold'
-    textList[0].color = (0,0,0)
-    textList[0].size = 33
-    textList[0].alignment = conn.ui.TextAnchor.lower_center
-    textList[0].rect_transform.position = (0,rect.size[1]/2-textList[0].size/2)
-
-    return (canvas,panel,textList)
+    mainMenu = Panel(conn, (500,750), (0,0), 'Main Menu')
 
 conn = krpc.connect(address='192.168.1.27')
 vessel = conn.space_center.active_vessel
